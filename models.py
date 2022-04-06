0b97251d8c4d5a7ba95157c5a0aa56fdaf40f371
ali13 <ali13@student.gsu.edu>
2022-04-06 00:01:26 -0400

    preliminary login functionality

---
+++ models.py
@@ -0,0 +1,16 @@
+import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+
+app = flask.Flask(__name__)
+app.secret_key = "Secret"
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+
+
+db.create_all()
