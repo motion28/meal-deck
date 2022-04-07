9bf379965b557db26c46e42fb0e90c6e5239a08a
KirtanK99 <kabariyakirtan99@gmail.com>
2022-04-07 15:47:43 -0400

    Getting all the required information from the API

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ load_dotenv(find_dotenv())
 app = flask.Flask(__name__)
 
 BASE_URL = "https://api.spoonacular.com/recipes/complexSearch"
+ING_URL = "https://api.spoonacular.com/recipes/"
 
 
 query_params = {
     "apiKey": os.getenv("SPOON_key"),
-    "query": "burger",
+    "query": "pasta",
     "instructionsRequired": "true",
     "addRecipeInformation": "true",
 }
@@ -25,29 +26,39 @@ response = requests.get(BASE_URL, params=query_params)
 recipes = response.json()
 recipe = recipes["results"][0]
 recipe_id = recipe["id"]
+
+# Getting the ingredients for the recipe from a different URL
+ingredients_touse = requests.get(
+    ING_URL
+    + str(recipe_id)
+    + "/information?apiKey="
+    + os.getenv("SPOON_key")
+    + "&includeNutrition=false"
+)
+
+ingredients = ingredients_touse.json()
+actual_ingredients = ingredients["extendedIngredients"]
+
+
+
 recipe_title = recipe["title"]
 recipe_image = recipe["image"]
 recipe_time = recipe["readyInMinutes"]
 recipe_steps = recipe["analyzedInstructions"][0]["steps"]
-steps = list(recipe_steps)
 
-for step in steps:
-    actual_steps=(step["step"])
-    print(actual_steps)
 
-#return all steps of the recipe
-# recipe_sum = recipe["summary"]
 
-@app.route("/")
-def index():
+@app.route("/", methods=["GET", "POST"])
+def all_recipes():
+    """ Prints out all information for the recipe being searched """
     return flask.render_template(
         "index.html",
-        recipe=recipe,
         recipe_title=recipe_title,
         recipe_time=recipe_time,
         recipe_image=recipe_image,
-        actual_steps=actual_steps,
-        # recipe_sum=recipe_sum,
+        recipe_steps=recipe_steps,
+        actual_ingredients=actual_ingredients,
     )
+
 
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
