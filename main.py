be81993dbb0b124714e2673dd86f2841c588454c
Moumin Ali <mouminiaali@Moumins-MacBook-Pro.local>
2022-04-08 16:29:26 -0400

    heroku setup

--- main.py
+++ main.py
@@ -147,4 +147,4 @@ def get_food():
     )
 app.run(debug=True)
 
-#app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
+#app.run(host='0.0.0.0', port=int(os.getenv("PORT", 8080)), debug=True)
