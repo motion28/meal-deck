84f7e63256531b2915e00569aee2d7d0969ca8f3
Moumin Ali <mouminiaali@Moumins-MacBook-Pro.local>
2022-04-08 21:51:07 -0400

    heroku setup

--- main.py
+++ main.py
@@ -180,6 +180,6 @@ def get_food():
     )
 
 port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+app.run(host='127.0.0.1', port=port)
 
 # app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
