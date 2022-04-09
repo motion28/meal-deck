c27433ab5f05bd55f336ca4ef82a03586563e598
Moumin Ali <mouminiaali@Moumins-MacBook-Pro.local>
2022-04-08 22:12:08 -0400

    heroku setup

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ flow = Flow.from_client_secrets_file(
         "https://www.googleapis.com/auth/userinfo.email",
         "openid",
     ],
-    redirect_uri="http://127.0.0.1:5000/callback",
+    redirect_uri="https://pacific-springs-45744.herokuapp.com/auth/google/callback",
 )
 
 
